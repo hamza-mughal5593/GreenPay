290ee7387cc705993001e247023a7cfcb07e22f4
hamza.mughal5593 <hamza.mughal5593@gmail.com>
2022-06-30 19:13:29 +0500

    30 -06-22

--- app/src/main/java/com/machineries_pk/mrk/activities/NewCode/CalculateResultActivity.kt
+++ app/src/main/java/com/machineries_pk/mrk/activities/NewCode/CalculateResultActivity.kt
@@ -1,15 +1,49 @@
 package com.machineries_pk.mrk.activities.NewCode
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.machineries_pk.mrk.R
 import com.machineries_pk.mrk.databinding.ActivityCalculateResultBinding
+import io.paperdb.Paper
 
-class CalculateResultActivity : AppCompatActivity() {
+class CalculateResultActivity : AppCompatActivity(), OnMapReadyCallback {
+    private var mMap: GoogleMap? = null
     lateinit var binding: ActivityCalculateResultBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityCalculateResultBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+        val mapFragment: SupportMapFragment? = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(this)
+
+
+    }
+
+    override fun onMapReady(p0: GoogleMap) {
+        mMap = p0
+        p0.getUiSettings()
+        // Add a marker in Sydney and move the camera
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(0.0, 0.0)
+//        mMap.addMarker(new MarkerOptions()
+//                .position(sydney)
+//                .title("Marker in Sydney"));
+        //        mMap.addMarker(new MarkerOptions()
+//                .position(sydney)
+//                .title("Marker in Sydney"));
+        mMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 18f))
+
+
+
+        mMap?.mapType = GoogleMap.MAP_TYPE_SATELLITE
+
+        p0.uiSettings.setAllGesturesEnabled(true)
     }
 }
--- app/src/main/java/com/machineries_pk/mrk/activities/NewCode/SignupActivity.kt
+++ app/src/main/java/com/machineries_pk/mrk/activities/NewCode/SignupActivity.kt
@@ -1,24 +1,33 @@
 package com.machineries_pk.mrk.activities.NewCode
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.BitmapFactory
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.machineries_pk.mrk.databinding.ActivitySignupBinding
+import pl.aprilapps.easyphotopicker.DefaultCallback
+import pl.aprilapps.easyphotopicker.EasyImage
+import pl.aprilapps.easyphotopicker.EasyImage.ImageSource
+import java.io.File
+import java.io.FileNotFoundException
 
 class SignupActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     lateinit var binding: ActivitySignupBinding
 
-    var courses = arrayOf<String?>("Select Account Type", "Account Type 1",
+    var courses = arrayOf<String?>(
+        "Select Account Type", "Account Type 1",
         "Account Type 2", "Account Type 3",
-        "Account Type 4", "Account Type 5")
+        "Account Type 4", "Account Type 5"
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding= ActivitySignupBinding.inflate(layoutInflater)
+        binding = ActivitySignupBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.signupBtn.setOnClickListener {
@@ -29,6 +38,16 @@ class SignupActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
 
 
+        binding.selectprofile.setOnClickListener {
+            EasyImage.openChooserWithGallery(
+                this,
+                "Select an image or make new one",
+                0
+            )
+
+        }
+
+
         binding.accountype.onItemSelectedListener = this
 
         // Create the instance of ArrayAdapter
@@ -36,28 +55,66 @@ class SignupActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
         val ad: ArrayAdapter<*> = ArrayAdapter<Any?>(
             this,
             android.R.layout.simple_spinner_item,
-            courses)
+            courses
+        )
 
         // set simple layout resource file
         // for each item of spinner
         ad.setDropDownViewResource(
-            android.R.layout.simple_spinner_dropdown_item)
+            android.R.layout.simple_spinner_dropdown_item
+        )
 
         // Set the ArrayAdapter (ad) data on the
         // Spinner which binds data to spinner
         binding.accountype.adapter = ad
 
     }
-    override fun onItemSelected(parent: AdapterView<*>?,
-                                view: View, position: Int,
-                                id: Long) {
+
+    override fun onItemSelected(
+        parent: AdapterView<*>?,
+        view: View, position: Int,
+        id: Long
+    ) {
         // make toastof name of course
         // which is selected in spinner
-        Toast.makeText(applicationContext,
+        Toast.makeText(
+            applicationContext,
             courses[position],
-            Toast.LENGTH_LONG)
+            Toast.LENGTH_LONG
+        )
             .show()
     }
 
     override fun onNothingSelected(parent: AdapterView<*>?) {}
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        EasyImage.handleActivityResult(
+            requestCode,
+            resultCode,
+            data,
+            this,
+            object : DefaultCallback() {
+                override fun onImagePickerError(e: Exception, source: ImageSource, type: Int) {
+                    super.onImagePickerError(e, source, type)
+                }
+
+                override fun onImagesPicked(
+                    imageFiles: List<File>,
+                    source: ImageSource,
+                    type: Int
+                ) {
+                    try {
+                        val filePath = imageFiles[0].path
+//                        mPaths.add(filePath)
+                        binding.profileImage.setImageBitmap(BitmapFactory.decodeStream(contentResolver.openInputStream(Uri.fromFile(imageFiles[0]))
+                            )
+                        )
+                    } catch (e: FileNotFoundException) {
+                        e.printStackTrace()
+                    }
+                }
+            })
+    }
 }
