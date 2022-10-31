3c0a6164e6dccea367bcdf561ae7509f8305fa59
hamzanawaz <hamza.mughal5593@gmail.com>
2022-10-31 12:40:37 +0500

    31-10-22

---
+++ app/src/main/java/com/machineries_pk/mrk/activities/Utils/ProgressBarAnimation.java
@@ -0,0 +1,42 @@
+package com.machineries_pk.mrk.activities.Utils;
+
+import android.content.Context;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+public class ProgressBarAnimation extends Animation {
+
+    public Context context;
+    private ProgressBar progressBar;
+    private TextView textView;
+    private float from;
+    private float to;
+
+
+    public ProgressBarAnimation(Context context, ProgressBar progressBar, TextView textView, float from, float to) {
+        this.context = context;
+        this.progressBar = progressBar;
+        this.textView = textView;
+        this.from = from;
+        this.to = to;
+
+    }
+
+    @Override
+    protected void applyTransformation(float interpolatedTime, Transformation t) {
+        super.applyTransformation(interpolatedTime, t);
+
+        float value = from + (to - from) * interpolatedTime;
+        progressBar.setProgress((int) value);
+        if (textView!=null)
+            textView.setText((int) value + " %");
+
+//            if (value == to )
+//            {
+//                startActivity();
+//            }
+
+    }
+}
